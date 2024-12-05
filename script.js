d46ceb903b4278a550be316634126d88849b502c
spinner99-bit <121241376+spinner99-bit@users.noreply.github.com>
2024-12-06 05:18:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
-
-document.addEventListener('keydown', function (e) {
-    if (e.keyCode == 123) { // F12
-        e.preventDefault();
-    }
-});
-
 // 检查用户登录状态
 function checkLoginStatus() {
     const username = localStorage.getItem('username');
